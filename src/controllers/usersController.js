bf3874199e734de5fc6154789b67c4187708fdec
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-01-31 17:54:43 +0300

    expanded the user content

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,6 +3,8 @@ const { body, validationResult, matchedData } = require("express-validator");
 
 const alphaErr = "must only contain letters.";
 const lengthErr = "must be between 1 and 10 characters.";
+const emailErr = "must be an email -> something@mail.com"
+const ageErr = "must be a number, why write it out?"
 
 const validateUser = [
   body("firstName").trim()
@@ -10,7 +12,11 @@ const validateUser = [
     .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
   body("lastName").trim()
     .isAlpha().withMessage(`Last name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+  body("email").trim().optional().isEmail().withMessage(`Email ${emailErr}`),
+  body("age").trim().optional().isLength({ min: 18, max: 120}),
+  body("bio").trim().optional().isLength({ max: 200 })
+  
 ];
 
 exports.usersCreatePost = [
@@ -23,8 +29,8 @@ exports.usersCreatePost = [
         errors: errors.array(),
       })
     }
-    const { firstName, lastName } = matchedData(req);
-    usersStorage.addUser({ firstName, lastName });
+    const { firstName, lastName, email, age, bio } = matchedData(req);
+    usersStorage.addUser({ firstName, lastName, email, age, bio });
     res.redirect("/");
   }
 ]
@@ -62,8 +68,9 @@ exports.usersUpdatePost = [
         errors: errors.array(),
       });
     }
-    const { firstName, lastName } = matchedData(req);
-    usersStorage.updateUser(req.params.id, { firstName, lastName });
+    const { firstName, lastName, email, age, bio } = matchedData(req);
+    usersStorage.updateUser(req.params.id, { firstName, lastName, email, age,
+      bio });
     res.redirect("/");
   }
 ];
